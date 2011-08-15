fa0752268c27f4c6a547ee0f57464fa2c0d43cc4
simon.andresen <Simon.Royrvik.Andresen@iteraconsulting.no>
2011-08-15 16:41:34 +0200

    Reset highscore key in settings menu

--- src/no/andsim/bubbles/activity/SettingsMenu.java
+++ src/no/andsim/bubbles/activity/SettingsMenu.java
@@ -2,18 +2,23 @@ package no.andsim.bubbles.activity;
 
 import no.andsim.bubbles.listener.OnOffRadioChangeListener;
 import no.andsim.bubbles.model.Settings;
+import no.andsim.bubbles.persistence.KeyValueRepository;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
 public class SettingsMenu extends Activity {
 
 	private OnOffRadioChangeListener radioSettingListener;
-	
+	private KeyValueRepository keyValueRepository;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		keyValueRepository = new KeyValueRepository(this.getApplicationContext());
 		setContentView(R.layout.settings);
 		setInitialState();
 		radioSettingListener = new OnOffRadioChangeListener();
@@ -23,29 +28,43 @@ public class SettingsMenu extends Activity {
 		vibrateRadioGrp.setOnCheckedChangeListener(radioSettingListener);
 		RadioGroup devRadioGrp = (RadioGroup) findViewById(R.id.groupDev);
 		devRadioGrp.setOnCheckedChangeListener(radioSettingListener);
+		Button clearHigh = (Button) findViewById(R.id.clearHigh);
+		clearHigh.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				keyValueRepository.saveInt("highscore", 0);
+				
+			}
+		});
 	}
 
 	private void setInitialState() {
-		
-		//Sound
+
+		// Sound
 		RadioButton soundOn = (RadioButton) findViewById(R.id.soundOnRadio);
-		if(Settings.isSound())soundOn.setChecked(true);
+		if (Settings.isSound())
+			soundOn.setChecked(true);
 		RadioButton soundOff = (RadioButton) findViewById(R.id.soundOffRadio);
-		if(!Settings.isSound())soundOff.setChecked(true);
-		
-		//Vibration
+		if (!Settings.isSound())
+			soundOff.setChecked(true);
+
+		// Vibration
 		RadioButton vibrateOn = (RadioButton) findViewById(R.id.vibrateOnRadio);
-		if(Settings.isVibration())vibrateOn.setChecked(true);
+		if (Settings.isVibration())
+			vibrateOn.setChecked(true);
 		RadioButton vibrateOff = (RadioButton) findViewById(R.id.vibrateOffRadio);
-		if(!Settings.isVibration())vibrateOff.setChecked(true);
-		
-		//Devmode
+		if (!Settings.isVibration())
+			vibrateOff.setChecked(true);
+
+		// Devmode
 		RadioButton devOn = (RadioButton) findViewById(R.id.devOnRadio);
-		if(Settings.isDevmode())devOn.setChecked(true);
+		if (Settings.isDevmode())
+			devOn.setChecked(true);
 		RadioButton devOff = (RadioButton) findViewById(R.id.devOffRadio);
-		if(!Settings.isDevmode())devOff.setChecked(true);
-		
-		
+		if (!Settings.isDevmode())
+			devOff.setChecked(true);
+
 	}
 
 }
